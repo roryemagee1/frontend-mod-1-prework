218d6f0125c34270e6b8d553a1401300e04395b5
Rory Magee <roryemagee@gmail.com>
2021-10-27 21:48:22 -0600

    Complete section3

---
+++ section3/arrayMethods.md
@@ -0,0 +1,9 @@
+There are several methods for adding and removing elements to an array.  Here are four basic operations for doing so below.
+
+push - This operation adds a selected element to the end of an array.
+
+pop - This operation removes a selected element from the end of an array.
+
+shift - This operation removes a selected element from the beginning of an array.
+
+unshift - This operation adds a selected element to the beginning of an array.
--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,53 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
+console.log(animals.length);
 
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals.pop("Elephant");
+animals.push("Gorilla");
+console.log(animals);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.splice(2, 0, "Platypus");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.push("Elephant");
+console.log(animals);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ["Pizza", "Icecream", "Vegetables", "Steak"];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push("Broccoli");
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop("Broccoli");
+console.log(foods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.push("Rice", "Beans", "Chocolate");
+console.log(foods);
 
 // YOU DO: Remove the food that is in index position 0.
+foods.splice(0, 1);
+console.log(foods);
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +90,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Pinterest probably uses arrays to store pictures.
+// 2: WhatsApp probably uses arrays to store contacts.
+// 3: Lyft probably uses arrays to store the dates and times of your rides.
 
 
 //-------------------
@@ -96,9 +107,9 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
-The can call an uber if they have a charger and it is a car charger.
+They can call an uber if they have a charger and it is a car charger.
 
 */
 var percentBatteryLeft = 12;
@@ -106,6 +117,16 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+
+// The first line starts with an if statement with an OR conditional checking whether the user has either 15% power remaining or if they have a car charger.  Since they must specifically have a car charger, I added a nested AND conditional checking both that the user has a charger and that it is a car charger.  The statement checks if the OR statement with the nest AND conditional on one side evaluates to true.
+if (percentBatteryLeft >= 15 || (hasCharger === true && chargerType === "car")) {
+  // If the if statement evaluates to true, the following console log line is executed, and the console prints the string below indicating that the user can call an uber.
+  console.log("This user can call an uber.");
+// Since the if statement above encompasses all situations where the user is able to call an uber, I used an else statement to encompass all other situtations where the user cannot call an uber.
+} else {
+  // If the if statement evaluates false, the else statement kicks in and the following console log command prints out the string below indicating that the user cannot call an uber.
+  console.log("This user cannot call an uber.");
+}
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
